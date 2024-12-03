1de902f7cf26b7b590aee35dd2a60d9cbf41629b
Balvorn <leo-marcou@hotmail.fr>
2024-12-03 15:47:15 +0100

    simple heart rate monitor

---
+++ app/PulseIndicator.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+import { useDerivedValue } from "react-native-reanimated";
+import { View } from "react-native";
+
+const PulseIndicator = () => {
+
+  const interval = 1250;
+
+
+
+  return (
+ "2"
+  );
+};
+
+export default PulseIndicator
